ec2e8ffcca76e83f2cc1c5dd4f400f289f9fe083
Halil Tarık ELBİR <ayvakurbisi@gmail.com>
2023-05-06 20:11:35 +0300

    Added player movement and ball shooting

---
+++ FootballGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float movDirection = 1.0f;
+    public GameObject projectilePrefabs;
+    private float xRange = 10.0f;
+    private float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x<-xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            movDirection *= -1;
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            movDirection *= -1;
+        }
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * movDirection);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(projectilePrefabs, transform.position, projectilePrefabs.transform.rotation);
+        }
+    }
+}
